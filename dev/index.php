243c0082680dd0ebd3f9feeec7ef063b7a03797d
a23jorguzpla <a23jorguzpla@inspedralbes.cat>
2026-04-24 12:55:43 +0200

    Pagina principal amb bootstrap

--- dev/index.php
+++ dev/index.php
@@ -1,33 +1,49 @@
-<?php
-// Puedes poner lógica PHP aquí si lo necesitas
-$titulo = "Mi primera página en PHP";
-?>
+<?php $titulo = "Pagina d'incidències"; ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <title><?php echo $titulo; ?></title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            margin-top: 50px;
-        }
-        h1 {
-            color: #333;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
+
 <body>
+    <header>
 
-    <h1><?php echo $titulo; ?></h1>
-    <p>Bienvenido a mi página web hecha con PHP.</p>
+    </header>
 
-    <?php
-    // Ejemplo de contenido dinámico
-    echo "<p>Hoy es: " . date("d/m/Y") . "</p>";
-    ?>
+    <nav>
+        <div class="row">
+                <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+                    <div class="card-body">
+                        <h5 class="card-title">Usuari</h5>
+                        <p class="card-text">Podràs crear incidencies i consultar el seu estat</p>
+                        <a href="#" class="btn btn-primary">Accedeix</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+                <div class="card-body">
+                    <h5 class="card-title">Tècnic</h5>
+                    <p class="card-text">Podràs accedir a les teves incidències assignades i fer modificacions</p>
+                    <a href="#" class="btn btn-primary">Accedeix</a>
+                </div>
+            </div>
+            <div class="card shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+                <div class="card-body">
+                    <h5 class="card-title">Administrador</h5>
+                    <p class="card-text">Podràs accedir al llistat d'incidencies per administrar-les i veure informes</p>
+                    <a href="#" class="btn btn-primary">Accedeix</a>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
 
+    </nav>
 </body>
+
 </html>
