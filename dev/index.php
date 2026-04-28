20f1789700fc7327b8b21cad21daac1d9466402a
a23jorguzpla <a23jorguzpla@inspedralbes.cat>
2026-04-28 14:16:59 +0200

    he afegit un link per anar a crear.php

--- dev/index.php
+++ dev/index.php
@@ -42,7 +42,7 @@
                                     Accedir
                                 </button>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="crear.php">Crear incidència</a></li>
                                     <li><a class="dropdown-item" href="#">Another action</a></li>
                                     <li><a class="dropdown-item" href="#">Something else here</a></li>
                                 </ul>
