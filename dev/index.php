79f296ee3a9321312057358c41b3cbf326a4cf71
a23jorguzpla <a23jorguzpla@inspedralbes.cat>
2026-04-28 14:15:08 +0200

    he millorat la capçalera

--- dev/index.php
+++ dev/index.php
@@ -11,15 +11,27 @@
 
 <body>
     <header>
-        <div class="bg-dark text-white p-5 mb-5 shadow-lg">
-        <div class="d-flex justify-content-center fs-1">Pagina de gestió d'incidències</div>
-        <div class="d-flex justify-content-center fs-5">GRUP4: Ramses i Jordi</div>
-    </div>
+        <div class="bg-dark text-white p-2 mb-2 shadow-lg">
+            <div class="container text-center">
+            <div class="row">
+                <div class="col-2">
+                    <img class="img-fluid" style="width: 80px;" src="https://lh3.googleusercontent.com/sitesv/AA5AbUD5FqRdWpu9LzNLp_qxjenHkBb6gIg0-WuDgrrFLCrLVt_kXMiNXY4nKx5ywk84vV9-BG4sJHTBs-CO_O6iXxKC_iLlvajPxyL4zTj2ksM0_l2Gic3hq8s9dSyieFwas4xa8wiScF50XWwqiSkooYCLNiB5v3NLDe7BdpuxfZF7iPLTi37zAXGPfGPZ3FqWg4D2PCoWMx4ttPCM00t_eMzYMedPuTe2SelT=w1280" alt="">
+                </div>
+                <div class="col-8">
+                    <div class="fs-1">GESTOR D'INCIDÈNCIES</div>
+                </div>
+                <div class="col-2">
+                    <div class="fs-6 pt-3">GRUP4: Ramses i Jordi</div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
     </header>
     <main>
         <div class="container text-center">
             <div class="row">
-                
+
                 <div class="col">
                     <div class="card" style="width: 18rem;">
                         <img src="https://static.vecteezy.com/system/resources/thumbnails/059/153/869/small/3d-rendered-blue-icon-with-a-white-user-symbol-representing-modern-digital-identity-and-communication-on-transparent-background-png.png" class="card-img-top" alt="...">
