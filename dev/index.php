c9be7117d75b61b25616df036539556bb7b1e3a7
a23jorguzpla <a23jorguzpla@inspedralbes.cat>
2026-04-29 12:10:44 +0200

    he canviat el index

--- dev/index.php
+++ dev/index.php
@@ -11,20 +11,28 @@
     <?php include_once "header.php"; ?>
     <main>
         <div class="card w-75 mb-3">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Button</a>
-  </div>
-</div>
+            <div class="card-body">
+                <h5 class="card-title">Profesor</h5>
+                <p class="card-text">Escull usuari</p>
+                <a href="#" class="btn btn-primary">Accedeix</a>
+            </div>
+        </div>
 
-<div class="card w-50">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Button</a>
-  </div>
-</div>
+        <div class="card w-50">
+            <div class="card-body">
+                <h5 class="card-title">Tècnic</h5>
+                <p class="card-text">Escull usuari</p>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Accedeix
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="estat.php">Juan</a></li>
+                        <li><a class="dropdown-item" href="estat.php">Maria</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <?php include_once "footer.php"; ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
