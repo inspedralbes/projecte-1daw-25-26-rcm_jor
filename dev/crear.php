c2e3dfba0c32e512f3bfe88f637748b6c4f77be5
a23jorguzpla <a23jorguzpla@inspedralbes.cat>
2026-04-29 10:32:39 +0200

    error corregits de crear.php

--- dev/crear.php
+++ dev/crear.php
@@ -1,4 +1,4 @@
-?php $titulo = "Crear Incidència"; ?>
+<?php $titulo = "Crear Incidència"; ?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -18,7 +18,7 @@
                         <img class="img-fluid" style="width: 80px;" src="https://lh3.googleusercontent.com/sitesv/AA5AbUD5FqRdWpu9LzNLp_qxjenHkBb6gIg0-WuDgrrFLCrLVt_kXMiNXY4nKx5ywk84vV9-BG4sJHTBs-CO_O6iXxKC_iLlvajPxyL4zTj2ksM0_l2Gic3hq8s9dSyieFwas4xa8wiScF50XWwqiSkooYCLNiB5v3NLDe7BdpuxfZF7iPLTi37zAXGPfGPZ3FqWg4D2PCoWMx4ttPCM00t_eMzYMedPuTe2SelT=w1280" alt="">
                     </div>
                     <div class="col-8">
-                        <div class="fs-1">Registrar INCIDÈNCIA</div>
+                        <div class="fs-1">REGISTRAR INCIDÈNCIA</div>
                     </div>
                     <div class="col-2">
                         <div class="fs-6 pt-3">GRUP4: Ramses i Jordi</div>
