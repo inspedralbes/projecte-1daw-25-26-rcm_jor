eb83268dfe4017185a095395138d05e55ea6712e
a23jorguzpla <a23jorguzpla@inspedralbes.cat>
2026-04-29 12:02:59 +0200

    he creat procesar.php per que les dades del formulari es guardin a la BBDD

--- dev/crear.php
+++ dev/crear.php
@@ -11,25 +11,11 @@
 
 <body>
     <header>
-        <div class="bg-dark text-white p-2 mb-2 shadow-lg">
-            <div class="container text-center">
-                <div class="row">
-                    <div class="col-2">
-                        <img class="img-fluid" style="width: 80px;" src="https://lh3.googleusercontent.com/sitesv/AA5AbUD5FqRdWpu9LzNLp_qxjenHkBb6gIg0-WuDgrrFLCrLVt_kXMiNXY4nKx5ywk84vV9-BG4sJHTBs-CO_O6iXxKC_iLlvajPxyL4zTj2ksM0_l2Gic3hq8s9dSyieFwas4xa8wiScF50XWwqiSkooYCLNiB5v3NLDe7BdpuxfZF7iPLTi37zAXGPfGPZ3FqWg4D2PCoWMx4ttPCM00t_eMzYMedPuTe2SelT=w1280" alt="">
-                    </div>
-                    <div class="col-8">
-                        <div class="fs-1">REGISTRAR INCIDÈNCIA</div>
-                    </div>
-                    <div class="col-2">
-                        <div class="fs-6 pt-3">GRUP4: Ramses i Jordi</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
+        <?php include("header.php"); ?>
     </header>
     <main>
         <div class="d-flex justify-content-center">
+            <h1>REGISTRAR INCIDENCIA</h1>
             <fieldset class="d-flex justiry-content-center border border-secondary rounded-2 p-5">
                 <form action="http://daw.inspedralbes.cat/form/action.php" method="post">
                     <label for="departament">Departament:</label> <br>
@@ -38,7 +24,7 @@
                     <input class="mb-3 form-control" name="Data" id="data" type="date"><br>
                     <label for="descripcio">Descripció:</label><br>
                     <textarea class="mb-3 form-control" name="Descripció" id="descripcio" rows="10"></textarea><br>
-                    <button type="submit" class="btn btn-primary">Enviar</button>
+                    <button href="index.php" type="submit" class="btn btn-primary">Enviar</button>
                 </form>
             </fieldset>
         </div>
