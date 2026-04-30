792f3a4fea4fbffce7abd81ea7b743ddf41e0f9e
a23jorguzpla <a23jorguzpla@inspedralbes.cat>
2026-04-30 09:26:29 +0200

    index.php modificat

--- dev/index.php
+++ dev/index.php
@@ -10,21 +10,40 @@
 <body>
     <?php include_once "header.php"; ?>
     <main>
-        <div class="card w-75 mb-3">
-            <div class="card-body">
-                <h5 class="card-title">Profesor</h5>
-                <p class="card-text">Escull usuari</p>
-                <a href="#" class="btn btn-primary">Accedeix</a>
-            </div>
-        </div>
-
         <div class="card w-50">
             <div class="card-body">
-                <h5 class="card-title">Tècnic</h5>
-                <p class="card-text">Escull usuari</p>
+                <h5 class="card-title">Profesor</h5>
                 <div class="dropdown">
                     <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Accedeix
+                        Escull usuari
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="crear.php">Juan</a></li>
+                        <li><a class="dropdown-item" href="estat.php">Maria</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="card w-50">
+            <div class="card-body">
+                <h5 class="card-title">Tecnic</h5>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Escull usuari
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="estat.php">Juan</a></li>
+                        <li><a class="dropdown-item" href="estat.php">Maria</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="card w-50">
+            <div class="card-body">
+                <h5 class="card-title">Administrador</h5>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Escull usuari
                     </button>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="estat.php">Juan</a></li>
