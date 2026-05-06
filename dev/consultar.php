274c49b93a0132a6c75c8e9805b099446cbb9656
a23jorguzpla <a23jorguzpla@inspedralbes.cat>
2026-05-06 10:34:54 +0200

    creada consultar.php
    Added a header section for the 'Consultar Incidències' page.

---
+++ dev/consultar.php
@@ -0,0 +1,14 @@
+<?php include_once "header.php";?>
+<header>
+    <div class="container-fluid bg-dark text-white p-2 mb-2 shadow-lg text-center">
+        <div class="row align-items-center">
+            <div class="col-2"></div>
+            <div class="col-8">
+                <div class="fs-1">Consultar Incidències</div>
+            </div>
+            <div class="col-2">
+                <div class="fs-6">GRUP 4: Ramses i Jordi</div>
+            </div>
+        </div>
+    </div>
+</header>
